40bc4260e440eebfb73e2d7dd1758500a45b705c
Jonatas Leon <jonatas.leon@gmail.com>
2019-04-20 16:33:43 -0300

    Fix install apt packages

--- install.sh
+++ install.sh
@@ -2,58 +2,62 @@
 
 i3DIR=$HOME/.config/i3-gaps
 
+packages_list=(
+    acpi
+    alttab
+    asciinema
+    dmenu
+    ffmpeg
+    git
+    git-svn
+    htop
+    hunspell
+    i3
+    i3blocks
+    i3lock
+    i3status
+    minidlna
+    rcm
+    scrot
+    traceroute
+    whois
+    xclip
+)
+
+i3_list=(
+    xcb
+    libxcb1-dev
+    libxcb-shape0-dev
+    libxcb-keysyms1-dev
+    libpango1.0-dev
+    libxcb-util0-dev
+    libxcb-icccm4-dev
+    libyajl-dev
+    libstartup-notification0-dev
+    libxcb-randr0-dev
+    libev-dev
+    libxcb-cursor-dev
+    libxcb-xinerama0-dev
+    libxcb-xkb-dev
+    libxkbcommon-dev
+    libxkbcommon-x11-dev
+    autoconf
+    libxcb-xrm0
+    libxcb-xrm-dev
+    automake
+)
+
 add_repositories() {
     sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
 }
 
 install_softwares() {
-    sudo apt-get install -y <<EOLIST
-        acpi
-        alttab
-        asciinema
-        dmenu
-        ffmpeg
-        git
-        git-svn
-        htop
-        hunspell
-        i3
-        i3blocks
-        i3lock
-        i3status
-        minidlna
-        rcm
-        scrot
-        traceroute
-        whois
-        xclip
-EOLIST
+    sudo apt-get install -y ${packages_list[@]}
 }
 
 
 install_i3() {
-    sudo apt-get install -y <<EOLIST
-        xcb
-        libxcb1-dev
-        libxcb-shape0-dev
-        libxcb-keysyms1-dev
-        libpango1.0-dev
-        libxcb-util0-dev
-        libxcb-icccm4-dev
-        libyajl-dev
-        libstartup-notification0-dev
-        libxcb-randr0-dev
-        libev-dev
-        libxcb-cursor-dev
-        libxcb-xinerama0-dev
-        libxcb-xkb-dev
-        libxkbcommon-dev
-        libxkbcommon-x11-dev
-        autoconf
-        libxcb-xrm0
-        libxcb-xrm-dev
-        automake
-EOLIST
+    sudo apt-get install -y ${i3_list[@]}
 
     # clone the repository
     git clone https://www.github.com/Airblader/i3 $i3DIR
