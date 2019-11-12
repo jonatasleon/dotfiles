b7028713e97e47b51d87d070c03bc27c13ab84c9
Jonatas Leon <jonatas.leon@gmail.com>
2019-11-12 11:50:16 -0300

    Update install script

--- install.sh
+++ install.sh
@@ -4,28 +4,27 @@ packages_list=$(cat ./packages)
 i3_list=$(cat ./i3packages)
 
 add_repositories() {
-    sudo add-apt-repository -y ppa:martin-frost/thoughtbot-rcm
-    sudo add-apt-repository -y ppa:neovim-ppa/stable
-    sudo apt-get update && sudo apt-get upgrade -y
+  sudo add-apt-repository -y ppa:martin-frost/thoughtbot-rcm
+  sudo add-apt-repository -y ppa:neovim-ppa/stable
+  sudo apt-get update && sudo apt-get upgrade -y
 }
 
 install_font() {
-    cd /tmp
-    curl -L -o icons.zip $1
-    mkdir -p $HOME/.fonts
-    unzip -l icons | grep -e ".*ttf$" | awk '{ print $4  }' | xargs unzip icons.zip -d $HOME/.fonts
-    rm icons.zip
+  $OUT=$(mktemp)
+  curl -L -o $OUT $1
+  mkdir -p $HOME/.fonts
+  unzip -l icons | grep -e ".*ttf$" | awk '{ print $4  }' | xargs unzip $OUT -d $HOME/.fonts
 }
 
 install_packages() {
-    sudo apt-get install -y $(echo $packages_list)
-    sudo apt-get install -y $(echo $i3_list)
+  sudo apt-get install -y $(echo $packages_list)
+  sudo apt-get install -y $(echo $i3_list)
 }
 
 install_i3() {
-    # clone the repository
-    git clone https://www.github.com/airblader/i3 /tmp/i3gaps
-    cd /tmp/i3gaps
+  # clone the repository
+  git clone https://www.github.com/airblader/i3 /tmp/i3gaps
+  cd /tmp/i3gaps
 
     # compile & install
     autoreconf --force --install
@@ -37,124 +36,138 @@ install_i3() {
     ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
     make
     sudo make install
-}
+  }
 
 install_betterlockscreen() {
-    mkdir -p $HOME/.local/bin/
-    wget -O $HOME/.local/bin/betterlockscreen https://git.io/fASUJ
-    chmod u+x $HOME/.local/bin/betterlockscreen
+  mkdir -p $HOME/.local/bin/
+  wget -O $HOME/.local/bin/betterlockscreen https://git.io/fASUJ
+  chmod u+x $HOME/.local/bin/betterlockscreen
 }
 
 install_i3lockcolor() {
-    git clone https://github.com/PandorasFox/i3lock-color.git /tmp/i3lock-color
-    cd /tmp/i3lock-color
+  git clone https://github.com/PandorasFox/i3lock-color.git /tmp/i3lock-color
+  cd /tmp/i3lock-color
 
-    autoreconf --force --install
-    rm -rf build/
-    mkdir -p build && cd build/
+  autoreconf --force --install
+  rm -rf build/
+  mkdir -p build && cd build/
 
     # Disabling sanitizers is important for release versions!
     # The prefix and sysconfdir are, obviously, dependent on the distribution.
     ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
     make
     sudo make install
-}
+  }
 
 install_xcbutils() {
-    cd /tmp/
-    git clone --recursive https://github.com/Airblader/xcb-util-xrm
-    cd xcb-util-xrm/
-    ./autogen.sh
-    make
-    sudo make install
+  cd /tmp/
+  git clone --recursive https://github.com/Airblader/xcb-util-xrm
+  cd xcb-util-xrm/
+  ./autogen.sh
+  make
+  sudo make install
 
-    echo '/usr/local/lib/' > /etc/ld.so.conf.d/i3.conf
-    sudo ldconfig
-    sudo ldconfig -p
+  echo '/usr/local/lib/' > /etc/ld.so.conf.d/i3.conf
+  sudo ldconfig
+  sudo ldconfig -p
 }
 
 install_polybar() {
-    cd /tmp/
-    git clone --recursive https://github.com/jaagr/polybar.git
-    cd polybar
-    yes | ./build.sh
+  cd /tmp/
+  git clone --recursive https://github.com/jaagr/polybar.git
+  cd polybar
+  yes | ./build.sh
+}
+
+install_pyenv() {
+  curl https://pyenv.run | bash
+  PY3_VERSION='3.8.0'
+  PY2_VERSION='2.7.15'
+  pyenv install $PY3_VERSION
+  pyenv install $PY2_VERSION
+
+  pyenv virtualenv $PY3_VERSION py3
+  pyenv virtualenv $PY3_VERSION nvim3
+
+  pyenv virtualenv $PY2_VERSION py2
+  pyenv virtualenv $PY2_VERSION nvim2
 }
 
 retrieve_dotfiles() {
-    git clone https://github.com/jonatasleon/dotfiles $HOME/.dotfiles
-    rcup -v
+  git clone https://github.com/jonatasleon/dotfiles $HOME/.dotfiles
+  rcup -v
 }
 
 post_config() {
-    echo 'Run the command above. Logout and login to finish.'
-    echo 'chsh -s $(which zsh)'
+  echo 'Run the command above. Logout and login to finish.'
+  echo 'chsh -s $(which zsh)'
 }
 
 script_name=$(basename $0)
 
 sub_help(){
-    echo "Usage: $script_name <subcommand> [options]"
-    echo "Subcommands:"
-    echo "    install-npm-packages  Install needed npm packages"
-    echo ""
-    echo "For help with each subcommand run:"
-    echo "$script_name <subcommand> -h|--help"
-    echo ""
-
+  echo "Usage: $script_name <subcommand> [options]"
+  echo "Subcommands:"
+  echo "    install-npm-packages  Install needed npm packages"
+  echo ""
+  echo "For help with each subcommand run:"
+  echo "$script_name <subcommand> -h|--help"
+  echo ""
 }
 
 sub_full-install() {
-    add_repositories
-    install_packages
-    install_xcbutils
-    install_i3
-    install_i3lockcolor
-    install_betterlockscreen
+  add_repositories
+  install_packages
+  install_pyenv
+  install_xcbutils
+  install_i3
+  install_i3lockcolor
+  install_betterlockscreen
 
-    # Install FontAwesome icons
-    install_font 'https://github.com/FortAwesome/Font-Awesome/releases/download/5.8.1/fontawesome-free-5.8.1-desktop.zip'
+  # Install FontAwesome icons
+  install_font 'https://github.com/FortAwesome/Font-Awesome/releases/download/5.8.1/fontawesome-free-5.8.1-desktop.zip'
 
-    # Install HelveticaNeue
-    install_font 'https://freefontsdownload.net/download/74318/helveticaneue.zip'
-    retrieve_dotfiles
-    post_config
+  # Install HelveticaNeue
+  install_font 'https://freefontsdownload.net/download/74318/helveticaneue.zip'
+  retrieve_dotfiles
+  post_config
 }
 
 sub_install-deb-packages() {
-    add_repositories
-    install_packages
+  add_repositories
+  install_packages
 }
 
 sub_install-npm-packages() {
-    npm_packages=(
-        eslint
-        prettier
-        csslint
-        jsonlint
-    )
-    param=$1
-    case $param in
-        "-h" | "--help")
-            echo "install npm packages"
-            ;;
-        "")
-            npm install -g ${npm_packages[@]}
-            ;;
-    esac
+  npm_packages=(
+    eslint
+    prettier
+    csslint
+    jsonlint
+  )
+  param=$1
+  case $param in
+    "-h" | "--help")
+      echo "install npm packages"
+      ;;
+    "")
+      npm install -g ${npm_packages[@]}
+      ;;
+  esac
 }
 
 subcommand=$1
 case $subcommand in
-    "" | "-h" | "--help")
-        sub_help
-        ;;
-    *)
-        shift
-        sub_${subcommand} $@
-        if [ $? = 127  ]; then
-            echo "Error: '$subcommand' is not a known subcommand." >&2
-            echo "       Run '$script_name --help' for a list of known subcommands." >&2
-            exit 1
-        fi
-        ;;
+  "" | "-h" | "--help")
+    sub_help
+    ;;
+  *)
+    shift
+    sub_${subcommand} $@
+    if [ $? = 127  ]; then
+      echo "Error: '$subcommand' is not a known subcommand." >&2
+      echo "       Run '$script_name --help' for a list of known subcommands." >&2
+      exit 1
+    fi
+    ;;
 esac
