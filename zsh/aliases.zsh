fadb921ebc1c17bcc378898ffe09d8604cb5b3e9
Jonatas Leon <jonatas.leon@gmail.com>
2020-06-24 11:20:16 -0300

    Update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,28 +1,35 @@
 # Set personal aliases, overriding those provided by oh-my-zsh libs,
 # plugins, and themes.
 # For a full list of active aliases, run `alias`.
-alias chrome="google-chrome-stable"
-alias fun="ts $(whoami) || ta $(whoami)"
+
+# Git-SVN aliases
 alias gsf="git svn fetch"
+
+# Make shell easier
 alias hs="history | grep"
 alias lhalt="ls -halt"
 alias lhaltr="ls -haltr"
 alias mkcd='foo() { mkdir -p "$1"; cd "$1" }; foo'
-alias mknote="touch note_$(date +%Y%m%d).md"
 alias more="less"
-alias profileconfig="$EDITOR ~/.dotfiles/profile.zsh"
 alias tailf="tail -f"
+alias xopen="xdg-open"
+
+# Updates
 alias update="sudo apt-get update && sudo apt-get upgrade -y"
 alias updatedotfiles="(cd ~/.dotfiles; git pull --rebase origin master)"
 alias updatesnips="(cd ~/.vim/UltiSnips; git pull --rebase origin master)"
-alias vim="nvim"
+
+# Edit Configs
+alias profileconfig="$EDITOR ~/.dotfiles/zsh/profile.zsh"
 alias vimconfig="$EDITOR ~/.dotfiles/config/nvim/init.vim"
-alias xopen="xdg-open"
 alias zshconfig="$EDITOR ~/.dotfiles/zshrc"
 
+# TMUX Aliases
+alias tssh="tmux-cssh"
+
 # pyenv aliases
-alias venv="pyenv virtualenv"
 alias activate="pyenv activate"
 alias deactivate="pyenv deactivate"
 alias shell="pyenv shell"
+alias venv="pyenv virtualenv"
 alias which="pyenv which"
